3592b6c76237de8ce4329186401b94a92d6ec77f
Kanishk Garg <61554323+KanishkGar@users.noreply.github.com>
2022-12-29 15:59:46 -0600

    login with recoil state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,15 @@
 import { Input, Center , Stack, Button} from '@chakra-ui/react'
+import { useRecoilState } from "recoil";
+import { loggedInAtom } from "../atoms";
 
 export const Login = (props) => {
+    const [loggedIn, setLoggedIn] = useRecoilState(loggedInAtom);
+
     return (
         <Center width={"100vw"} height={"75vh"}>
             <Stack spacing={4} align='center'>
                 <Input placeholder='Input Login' size='lg' htmlSize={16} width='auto' />
-                <Button colorScheme='blue' variant='outline' onClick={(text) => {}}>
+                <Button colorScheme='blue' variant='outline' onClick={(text) => {setLoggedIn(true)}}>
                     Log in
                 </Button>
             </Stack>
