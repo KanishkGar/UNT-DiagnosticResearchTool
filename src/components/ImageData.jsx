7ff90f1eabe9d828e03cc2cad36ad6b649ef81ce
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-12-29 16:20:48 -0600

    margin

--- src/components/ImageData.jsx
+++ src/components/ImageData.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 export const ImageData = (props) => {
     const [show, setShow] = useState(false);
     return (<>
-        <Button disabled={show} onClick={() => { setShow(true) }}>{props.data.title}</Button>
+        <Button mb={2} disabled={show} onClick={() => { setShow(true) }}>{props.data.title}</Button>
         {show && <Box backgroundColor={'#f0f0f0'} p={3} borderRadius={10}>
             <Image src={props.data.data} />
         </Box>}
