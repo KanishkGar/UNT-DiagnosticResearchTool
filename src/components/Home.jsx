1c262aa39bb1af2692214e383f986c2304d76696
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-12-29 15:29:06 -0600

    added some structure

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+    const questions = [
+        {
+            title: "Age",
+            data: "15 years"
+        },
+        {
+            title: "CT Scan Image",
+            data: "https://blog.beekley.com/hubfs/Imported_Blog_Media/opacified.jpg"
+        }
+    ]
+    return (
+        <>
+            
+        </>
+    )
+}
